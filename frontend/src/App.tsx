6965842108efcbc0af944eeb5d983919b790f5e5
Abdulaziz Hamid <abdulaziz5536h@gmail.com>
2026-09-15 17:00:07 +0300

    commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+
+function App() {
+  
+
+  return (
+    <>
+    <Routes>
+
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,25 @@
+import "../styles.css";
+
+function Login(){
+
+  return(
+    <>
+    <div className="login">
+      <form>
+        <h1>Login</h1>
+        <input placeholder="example@mail.com" />
+        <br />
+        <input type="password" />
+
+      </form>
+
+    </div>
+    
+
+    </>
+      
+
+  )
+}
+
+export default Login;
